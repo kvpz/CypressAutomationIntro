f396d6b530c6ccb62ebeb00778b953a42fbf5dfb
kevin perez <unknotted26@gmail.com>
2020-02-13 10:25:06 -0500

    init

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,6 @@
+describe("My First Test Suite", function() {
+    // all test cases go in here
+    it("My First Test Case", function() {
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+    })
+})
